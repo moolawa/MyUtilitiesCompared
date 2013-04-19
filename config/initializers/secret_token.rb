15f9862bd4bcd93ebccf8e6e894cdcaa788bc147
The dolphin <moolawa@gmail.com>
2013-04-19 21:59:55 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyUtilitiesCompared::Application.config.secret_token = '36ffbd21622ec06be58a0d45c697b353456165411f05d53c6c3889d03e20b3e5ccd78401cc0ac848e45d0d62e5c8be4b6f7de7ff7abd7933e734fcca109d4e23'
